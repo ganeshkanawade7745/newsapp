06eb1701f434750e28fc695fe19adc755c9205fb
Ganesh <ganeshkanwade7745@gmail.com>
2022-03-30 14:00:33 +0530

    new2

--- src/Components/News.js
+++ src/Components/News.js
@@ -5,28 +5,45 @@ export default class News extends Component {
   render() {
     return (
         <>
-        < div className="container">
-            <div className="rows">
-                <div className="col-nd-4">
-                <Newsitems title="main news" description="this is news description"/>
- 
-                </div>
-                <div className="col-nd-4">
-                <Newsitems title="main news" description="this is news description"/>
- 
-                </div>
-                <div className="col-nd-4">
-                <Newsitems title="main news" description="this is news description"/>
- 
-                </div>
-            </div>
+           <div className="container my-3">
+           <div className="row">
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+           </div>
+   
+           <div className="row">
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+           </div>
+           <div className="row">
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+               <div className="col-md-4">
+                    <Newsitems title=" main News" description="this is news descriptions "/>
+               </div>
+           </div>
         
-      <div>this is news component</div>
-      <Newsitems title="main news" description="this is news description"/>
-      <Newsitems/>
-      <Newsitems/>
-      </div>
-      </>
-    )
-  }
-}
+        
+         </div>
+        </>
+       )
+     }
+   }
--- src/Components/Newsitems.js
+++ src/Components/Newsitems.js
@@ -10,7 +10,7 @@ export default class Newsitems extends Component {
     return (
      <>
       <div className="card" style={{width: '18rem'}}>
-          <img className="card-img-top" src="US intel assess 'major' strategy shift by Russia as it moves some forces away from Kyiv - CNN"alt="Card image cap" />
+          <img className="card-img-top" src="https://c.ndtvimg.com/2022-03/qs3qamco_ukraine-war-afp-650_625x300_08_March_22.jpg"alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
